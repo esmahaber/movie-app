7fa214737c03a419b722a3b155f482c74d20c29d
esmahaber <esma.haber@gmail.com>
2022-06-18 17:20:58 +0300

    add carousel

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 function Detail() {
   return (
     <div>Detail</div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,35 @@
 import { Link } from "react-router-dom"
+import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import films from '../helpers/films.json';
+import styled from 'styled-components';
 
 function Home() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex: number) => {
+    setIndex(selectedIndex);
+  };
+
   return (
     <div className="grid-body">
-        <h1>This is homepage</h1>
-         <Link to="/detail">Detail</Link>
+      <h1>This is homepage</h1>
+      <Carousel activeIndex={index} onSelect={handleSelect} interval={1000}>
+        {films.map((film, index) =>
+          <Carousel.Item key={index}>
+            <img
+              className="d-block w-100"
+              src={film.Images[0]}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>{film.Title}</h3>
+              <p>{film.Plot}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        )}
+
+      </Carousel>
     </div>
   )
 }
