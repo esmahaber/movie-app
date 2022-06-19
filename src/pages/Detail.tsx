d9eefccbfc83cb7296f41789ec70d5c8e8866238
esmahaber <esma.haber@gmail.com>
2022-06-19 13:10:43 +0300

    categories component and film detail

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,8 +1,79 @@
-import React from 'react'
+import { Badge, Card, Carousel } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import styled from 'styled-components';
+
+import films from '../helpers/films.json'
+
+function find(id: number) {
+  return films.find(film => film.Id === id);
+}
+
+const CardWrapper = styled.div`
+  margin:48px;
+  width: 100%;
+  @media (max-width: 768px) {
+    margin:48px 0;
+  }
+  .sub-info{
+    font-size:12px;
+  }
+  .card-title{
+    position: relative;
+    margin-top:5px;
+    .badge{
+      position:absolute;
+      right: 0;
+    }
+  }
+  .card-text{
+    margin-top:12px;
+  }
+  img{
+    max-height: 550px;
+    object-fit: cover;
+  }
+`;
 
 function Detail() {
+  var id: string = useParams().id!;
+  let film = find(parseInt(id));
+
   return (
-    <div>Detail</div>
+    <div className="grid-body">
+      <CardWrapper>
+        <Card style={{ width: '100%' }}>
+          <Card.Body>
+            <Carousel interval={null} indicators={false}>
+              {film?.Images.map(image =>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src={image}
+                    alt="First slide"
+                  />
+                  <Carousel.Caption>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              )}
+            </Carousel>
+            <Card.Title>{film?.Title}
+              <Badge bg="warning" text="dark">
+                IMDB {film?.imdbRating}
+              </Badge></Card.Title>
+            <div className="sub-info">
+              <div>{film?.Year} {film?.Runtime} </div>
+              <div>{film?.Genre}</div>
+              <div><b>Actors:</b> {film?.Actors}</div>
+
+            </div>
+            <Card.Text>
+              {film?.Plot}
+            </Card.Text>
+
+          </Card.Body>
+        </Card>
+      </CardWrapper>
+    </div>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
-import { Link } from "react-router-dom"
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import films from '../helpers/films.json';
 import styled from 'styled-components';
+import Card from 'react-bootstrap/Card';
+import categories from '../helpers/categories.json';
+import DummyCategoryImg from '../assets/images/dummy-categories-img.png';
+import { Link } from "react-router-dom";
 
 function Home() {
   const [index, setIndex] = useState(0);
@@ -11,27 +14,84 @@ function Home() {
     setIndex(selectedIndex);
   };
 
+  const CategoriesWrapper = styled.div`
+    display: flex;
+    overflow: hidden;  
+    overflow-x: scroll;
+    flex-wrap: inherit;
+    padding-left: 24px;
+    margin: 52px 0;
+  `;
+
+  const Category = styled.div`
+    grid-area: none;
+    flex: 0 0 auto;
+    max-height: 284px;
+    border-radius: 8px;
+    margin: 0px 5px;
+    width:40%;
+    @media (max-width: 768px) {
+      width:60%;
+    }
+    .card-title{
+      text-align:center;
+      font-size:32px;
+      letter-spacing: 12px;
+    }
+  `;
+
+  const CarouselLink = styled.a`
+  img{
+    height: 40vh;
+    object-fit: contain;
+  }
+`;
+  const GridBody = styled.div`
+  .carousel-caption{
+    background: #a0a0a04a;
+    padding:0;
+  }
+`;
+
   return (
-    <div className="grid-body">
-      <h1>This is homepage</h1>
-      <Carousel activeIndex={index} onSelect={handleSelect} interval={1000}>
-        {films.map((film, index) =>
-          <Carousel.Item key={index}>
-            <img
-              className="d-block w-100"
-              src={film.Images[0]}
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>{film.Title}</h3>
-              <p>{film.Plot}</p>
-            </Carousel.Caption>
+    <GridBody className="grid-body">
+      <Carousel activeIndex={index} onSelect={handleSelect} interval={4000}>
+        {films.map((film) =>
+          <Carousel.Item key={film.Id}>
+            <CarouselLink >
+              <Link to={`detail/${film.Id}`}>
+                <img
+                  className="d-block w-100"
+                  src={film.Poster}
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>{film.Title}</h3>
+                  <p>{film.Plot}</p>
+                </Carousel.Caption>
+              </Link>
+            </CarouselLink>
           </Carousel.Item>
         )}
-
       </Carousel>
-    </div>
+
+      <h6>Categories</h6>
+      <CategoriesWrapper>
+        {categories.map((category, index) =>
+          <Category key={index}>
+            <a href='#'>
+              <Card className="bg-dark text-white categories">
+                <Card.Img src={DummyCategoryImg} alt="Card image" />
+                <Card.ImgOverlay>
+                  <Card.Title>{category.name}</Card.Title>
+                </Card.ImgOverlay>
+              </Card>
+            </a>
+          </Category>
+        )}
+      </CategoriesWrapper>
+    </GridBody>
   )
 }
 
-export default Home
+export default Home;
